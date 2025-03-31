c0fd765addfceb04b905f9f5108971d4c6beef57
JamesBremner <james@ravenspoint.com>
2025-03-31 16:57:35 -0400

    Class interface declarations

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,87 @@
 #include <wex.h>
 #include "cStarterGUI.h"
 
+// virtual base class for all particles
+class particle
+{
+    public:
+    particle();
+    particle( int color );
+
+    // Each particle moves in a different way
+    // This method cannot be compiled
+    // but allows the correct method to run for the specialized particles
+    virtual void move() = 0;
+    
+    int color() const;
+
+    protected:
+    int myColor;
+    std::pair<int,int> myLocation;
+};
+
+// A grain of sand
+class grain : public particle
+{
+public:
+    grain();
+    virtual void move();
+};
+
+// A drop of water
+class water : public particle
+{
+public:
+    water();
+    virtual void move();
+};
+
+// A stone
+class stone : public particle
+{
+public:
+    stone();
+    virtual void move();
+};
+
+particle::particle()
+: myColor( 0x0000FF )
+{}
+particle::particle(int color)
+: myColor( color )
+{}
+
+int particle::color() const
+{
+    return myColor;
+}
+
+grain::grain()
+: particle( 0x00FFFF)
+{}
+void grain::move()
+{
+    myLocation.second++;
+}
+
+water::water()
+: particle(0xFF0000)
+{ }
+void water::move()
+{
+    myLocation.first++;
+}
+
+stone::stone()
+: particle(0x000000)
+{
+}
+void stone::move()
+{
+    myLocation.first--;
+}
+
+
 class cGUI : public cStarterGUI
 {
 public:
