cc5d1461d177f9d4317967d1b3ad5a5f865b7030
JamesBremner <james@ravenspoint.com>
2025-04-07 11:22:45 -0400

    document particle class interfaces

--- src/SimParticle.h
+++ src/SimParticle.h
@@ -25,6 +25,7 @@ public:
     particle();
     particle(int color, int x, int y);
 
+    // reset the grid to empty
     static void setGridSize();
 
     // Construct particle of required type
@@ -32,10 +33,11 @@ public:
         const wex::sMouse &m,
         int keyDown);
 
+    // refresh display of all particles
     static void drawAll(wex::shapes &S);
 
     /// @brief Move all particles that are not blocked
-    /// @return true if any particle moved ( display need to update )
+    /// @return true if any particle moved ( display needs to update )
     static bool moveAll();
 
     // Each particle moves in a different way
@@ -43,7 +45,7 @@ public:
     // but allows the correct method to run for the specialized particles
     virtual void move() = 0;
 
-    // move particle one cell
+    // move this particle one cell
     void moveDown();
     void moveLeft();
     void moveRight();
@@ -54,26 +56,33 @@ public:
     // get particle at grid location
     particle *get(int row, int col) const;
 
+    // particle blocked flag accessors
+    // blocked particles are skipped in move update ( performance optimization )
     void setAtRest(bool f = true);
     bool isAtRest() const;
 
+    // attribute accessors
     int color() const;
     std::pair<int, int> location() const;
 
+    // human readable status ( for debugging )
     virtual std::string text() const;
 
     // draw this particle
     void draw(wex::shapes &S) const;
 
+    // run the unit tests
     static bool test();
 
 protected:
+
     // attributes of this particle
+
     int myColor;
     std::pair<int, int> myLocation;
-    bool fAtRest;     // true if particle is blocked
-    bool myfMoveThis; // true if this particle moved
-    bool myfReplacedWater;  // true if particle is sinking through water
+    bool fAtRest;               // true if particle is blocked
+    bool myfMoveThis;           // true if this particle moved
+    bool myfReplacedWater;      // true if particle is sinking through water
 
     // static attributes shared by all particles
 
@@ -83,6 +92,14 @@ protected:
     // store the particles in their grid locations
     static grid_t theGrid;
 
+    /* Clear the fAtRest flags for any particles above
+
+    When a particle moves it will free particles above that were blocked
+
+    This only works properly if the grid is scanned from bottom to top
+    https://github.com/JamesBremner/SimParticle/issues/1#issuecomment-2781424962
+
+    */
     void freeGrainsAbove();
 
     /*  The move flags are set to false at start of each position update
